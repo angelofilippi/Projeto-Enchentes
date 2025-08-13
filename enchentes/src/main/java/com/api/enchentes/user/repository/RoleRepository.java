77b377d666434a175b39816e28af0a66f1de388f
angelofilippi <angelofilippih@gmail.com>
2025-08-12 21:41:20 -0300

    Adicionado lógica do repository das roles

--- enchentes/src/main/java/com/api/enchentes/user/repository/RoleRepository.java
+++ enchentes/src/main/java/com/api/enchentes/user/repository/RoleRepository.java
@@ -1,4 +1,10 @@
 package com.api.enchentes.user.repository;
 
-public class RoleRepository {
+import com.api.enchentes.user.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByRolename(String rolename);
 }
