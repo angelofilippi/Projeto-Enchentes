84f52daba525f8b8a28d4b667b5cc56aa1aa1836
Ângelo Henrique Filippi <angelofilippih@gmail.com>
2025-08-13 16:03:56 -0300

    Adicionado lógica de busca de usuario e role para o Spring Security

---
+++ enchentes/src/main/java/com/api/enchentes/config/security/UserDetailsServiceImpl.java
@@ -0,0 +1,30 @@
+package com.api.enchentes.config.security;
+
+import com.api.enchentes.user.entity.User;
+import com.api.enchentes.user.repository.UserRepository;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserRepository repo;
+
+    public UserDetailsServiceImpl(UserRepository repo) {
+        this.repo = repo;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User u = repo.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado"));
+        String role = u.getRole() != null ? u.getRole().getRolename() : "USUARIO";
+        return new org.springframework.security.core.userdetails.User(
+                u.getUsername(),
+                u.getPassword(),
+                List.of(new SimpleGrantedAuthority("ROLE_" + role))
+        );
+    }
+}
