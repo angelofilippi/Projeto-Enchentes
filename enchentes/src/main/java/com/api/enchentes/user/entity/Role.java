c3f83aa721ca52b1bc6f3fd30cdb92e32bc35c90
angelofilippi <angelofilippih@gmail.com>
2025-08-12 21:11:28 -0300

    Criado a entity das roles

--- enchentes/src/main/java/com/api/enchentes/user/entity/Role.java
+++ enchentes/src/main/java/com/api/enchentes/user/entity/Role.java
@@ -1,4 +1,21 @@
 package com.api.enchentes.user.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "roles")
 public class Role {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "rolename", nullable = false, unique = true, length = 50)
+    private String rolename;
+
+    public Long getId() { return id; }
+
+    public String getRolename() { return rolename; }
+
+    public void setId(Long id) { this.id = id; }
+
+    public void setRolename(String rolename) { this.rolename = rolename; }
 }
