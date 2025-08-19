71d51cc9c55162681cdf9e3aa38fd7a1e7e5760c
angelofilippi <angelofilippih@gmail.com>
2025-08-19 20:10:19 -0300

    feat: valida campos obrigatórios do registro

--- enchentes/src/main/java/com/api/enchentes/auth/dto/RegisterRequest.java
+++ enchentes/src/main/java/com/api/enchentes/auth/dto/RegisterRequest.java
@@ -1,3 +1,18 @@
 package com.api.enchentes.auth.dto;
 
-public record RegisterRequest(String username, String password) {}
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+public record RegisterRequest(@NotBlank(message = "E-mail é obrigatório")
+                              @Email(message = "Formato de e-mail inválido")
+                              String username,
+
+                              @NotBlank(message = "Senha é obrigatória")
+                              @Size(min = 11, message = "Senha deve ter mais de 10 caracteres")
+                              @Pattern(
+                                      regexp = "^(?=.*[!@#$%^&*()_+\\-\\[\\]{};':\\\"\\\\|,.<>/?]).+$",
+                                      message = "Senha deve conter ao menos 1 caractere especial"
+                              )
+                              String password) {}
