c3f2fcc631ab62fc73cae5300c1da2e3166a1dea
angelofilippi <angelofilippih@gmail.com>
2025-08-19 20:08:54 -0300

    feat: adiciona validação nas request

--- enchentes/src/main/java/com/api/enchentes/controller/AuthController.java
+++ enchentes/src/main/java/com/api/enchentes/controller/AuthController.java
@@ -2,6 +2,7 @@ package com.api.enchentes.controller;
 
 import com.api.enchentes.auth.dto.*;
 import com.api.enchentes.auth.service.AuthService;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,12 +14,12 @@ public class AuthController {
     public AuthController(AuthService service) { this.service = service; }
 
     @PostMapping("/register")
-    public ResponseEntity<AuthResponse> register(@RequestBody RegisterRequest req) {
+    public ResponseEntity<AuthResponse> register(@Valid @RequestBody RegisterRequest req) {
         return ResponseEntity.ok(service.register(req));
     }
 
     @PostMapping("/login")
-    public ResponseEntity<AuthResponse> login(@RequestBody AuthRequest req) {
+    public ResponseEntity<AuthResponse> login(@Valid @RequestBody AuthRequest req) {
         return ResponseEntity.ok(service.login(req));
     }
 }
