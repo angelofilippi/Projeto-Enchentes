2f5718c2b79200b9b8dad6f273714147230754a9
angelofilippi <angelofilippih@gmail.com>
2025-08-19 20:09:48 -0300

    feat: valida campos obrigatórios

--- enchentes/src/main/java/com/api/enchentes/auth/dto/AuthRequest.java
+++ enchentes/src/main/java/com/api/enchentes/auth/dto/AuthRequest.java
@@ -1,3 +1,15 @@
 package com.api.enchentes.auth.dto;
 
-public record AuthRequest(String username, String password) {}
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+public record AuthRequest(
+        @NotBlank(message = "E-mail é obrigatório")
+        @Email(message = "Formato de e-mail inválido")
+        String username,
+
+        @NotBlank(message = "Senha é obrigatória")
+        String password
+) {}
