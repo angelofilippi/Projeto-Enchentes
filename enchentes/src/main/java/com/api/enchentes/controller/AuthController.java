06add0a9a2ec44fdf081e24ffd3b64244e299c81
angelofilippi <angelofilippih@gmail.com>
2025-08-20 19:41:51 -0300

    feat: importa o swagger e documenta requests da API

--- enchentes/src/main/java/com/api/enchentes/controller/AuthController.java
+++ enchentes/src/main/java/com/api/enchentes/controller/AuthController.java
@@ -2,6 +2,9 @@ package com.api.enchentes.controller;
 
 import com.api.enchentes.auth.dto.*;
 import com.api.enchentes.auth.service.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,11 +16,23 @@ public class AuthController {
 
     public AuthController(AuthService service) { this.service = service; }
 
+    @Operation(description = "Registra um novo usuário")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Usuário registrado com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Dados inválidos ou já existentes"),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+    })
     @PostMapping("/register")
     public ResponseEntity<AuthResponse> register(@Valid @RequestBody RegisterRequest req) {
         return ResponseEntity.ok(service.register(req));
     }
 
+    @Operation(description = "Realiza o login de um usuário")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Login realizado com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Credenciais inválidas"),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+    })
     @PostMapping("/login")
     public ResponseEntity<AuthResponse> login(@Valid @RequestBody AuthRequest req) {
         return ResponseEntity.ok(service.login(req));
